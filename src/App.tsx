98e303568dd4935de3faca138303de732b0165c7
Alessandro Navarro Ishizawa <a.navarro.ishizawa@gmail.com>
2026-01-22 18:23:47 -0300

    feat: setup initial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>RT Movies</h1>
+      <p>Setup concluído com sucesso!</p>
+    </div>
+  )
+}
+
+export default App
